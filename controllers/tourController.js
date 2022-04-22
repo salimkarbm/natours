3e4b315188f150ac9d5a3f66eb121ec60fbba34f
salim imuzai <salimimuzai@gmail.com>
2022-04-22 20:31:13 +0100

    add route handller to fetch all tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,12 +1,27 @@
 const Tour = require('../models/tourModel');
 
-exports.createUser = async (req, res) => {
+exports.createTour = async (req, res) => {
   try {
     const newTour = await Tour.create({ ...req.body });
     res.status(201).json({
       status: 'success',
       data: {
         tours: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({ status: 'fail', message: err });
+  }
+};
+
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+    res.status(201).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours,
       },
     });
   } catch (err) {
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -4,6 +4,9 @@ const tourController = require('../controllers/tourController');
 
 const router = express.Router();
 
-router.route('/').post(tourController.createUser);
+router
+  .route('/')
+  .post(tourController.createTour)
+  .get(tourController.getAllTours);
 
 module.exports = router;
