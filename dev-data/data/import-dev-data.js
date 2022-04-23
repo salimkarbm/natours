754e042db021854f166d2fa5f3a184b0eb2b571c
salim imuzai <salimimuzai@gmail.com>
2022-04-23 19:26:18 +0100

    add script to load devdata

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+const Tour = require('../../models/tourModel');
+
+dotenv.config();
+
+let DB;
+if (process.env.NODE_ENV === 'development') {
+  DB = process.env.DATABASE;
+}
+mongoose.connect(DB, {}).then(() => {
+  console.log('db connection established');
+});
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf8')
+);
+
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data successfully deleted');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
