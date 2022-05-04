e858536538e8c7be524b2604ce6769c1e52b7da1
salim imuzai <salimimuzai@gmail.com>
2022-05-04 19:51:24 +0100

    add a global error handler

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 
 const tours = require('./routes/tourRoutes');
+const AppError = require('./utils/appError');
+const errorHandler = require('./controllers/errorControllers');
 
 const app = express();
 
@@ -9,11 +11,12 @@ app.use(express.json());
 app.use('/api/v1/tours', tours);
 
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `can't find ${req.originalUrl} on this server`,
-  });
-  next();
+  // const err = new Error(`can't find ${req.originalUrl} on this server!`);
+  // err.statusCode = 404;
+  // err.status = 'fail';
+  next(new AppError(`can't find ${req.originalUrl} on this server!`, 404));
 });
+
+app.use(errorHandler);
 
 module.exports = app;
