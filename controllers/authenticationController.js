cdbda4fead06442db499a47c9c9f792f20a6b1cf
salim imuzai <salimimuzai@gmail.com>
2022-05-09 10:03:16 +0100

    fixed the req body to allow only the specified fields

--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -3,7 +3,12 @@ const catchAsync = require('../utils/catchAsync');
 // const AppError = require('../utils/appError');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUder = await User.create(req.body);
+  const newUder = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+  });
 
   res.status(201).json({
     status: 'success',
