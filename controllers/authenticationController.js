36317f820fc2c897ff71d4d9cb29f1273ea89cfc
salim imuzai <salimimuzai@gmail.com>
2022-09-12 13:21:12 +0100

    store jwt token in http only

--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -12,7 +12,13 @@ const signToken = (id) => {
   });
 };
 const createSendToken = (user, statusCode, res) => {
+  const cookieOptions = {
+    expires:new Date(Date.now()) + process.env.JWT_COOKIES_EXPIRE_IN * 24 * 60 * 60 * 1000,
+    httpOnly:true
+  }
   const token = signToken(user._id);
+  if(process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  res.cookie('jwt', token, )
   return res.status(statusCode).json({
     status: 'success',
     token,
