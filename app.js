f647a039b895b0ff32c70408a43d8844cf3dd126
salim imuzai <salimimuzai@gmail.com>
2022-09-12 14:28:56 +0100

    prevent parameter pollution

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
-
+const helmet = require('helmet')
+const xss = require('xss-clean')
+const mongoSanitize = require('express-mongo-sanitize')
+const rateLimit = require('express-rate-limit')
+const hpp = require('hpp')
 const tours = require('./routes/tourRoutes');
 const users = require('./routes/userRoutes');
 const AppError = require('./utils/appError');
@@ -7,7 +11,27 @@ const errorHandler = require('./controllers/errorControllers');
 
 const app = express();
 
-app.use(express.json());
+// set security HTTP headers
+app.use(helmet())
+
+// limit request for APIs
+const limiter = rateLimit({
+  max:100,
+  windowMs:60*60*1000,
+  message:'Too many requests from this IP, please try again in an hour!'
+})
+app.use('/api', limiter)
+
+app.use(express.json({limit:'10kb'}));
+
+//DATA sanitazation against noSQL query injection
+app.use(mongoSanitize())
+
+// Data sanitazation against XSS
+app.use(xss())
+
+// prevent parameter pollution 
+app.use(hpp({whitelist:['duration', 'price','difficulty','ratingsAverage','maxGroupSize','ratingsQuantity']}))
 
 app.use('/api/v1/users', users);
 app.use('/api/v1/tours', tours);
